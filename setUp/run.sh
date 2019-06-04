9a5b87f818d1d1863fca5f5c6b3c654dc9f605a5
Koushal Kalal <koushal.kalal@tracxn.com>
2019-06-04 09:45:06 +0530

    Update the userId and password

--- setUp/run.sh
+++ setUp/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export DISPLAY=:0
 cd ~/GreytHRRegularization
-java -cp target/regularization-1.0.0.jar com.regularization.RegularizationService 873 beHappy20@
+java -cp target/regularization-1.0.0.jar com.regularization.RegularizationService userId password
