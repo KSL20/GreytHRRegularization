b0f73047936be115eb63285222c7f20989cf4003
Koushal Kalal <koushal.kalal@tracxn.com>
2019-06-20 08:09:02 +0530

    Update run command

--- setUp/run.sh
+++ setUp/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export DISPLAY=:0
 cd ~/GreytHRRegularization
-java -cp target/regularization-1.0.0.jar com.regularization.RegularizationService userId password
+java -cp target/regularization-1.0.0.jar com.regularization.RegularizationService userId password > /tmp/greythr.logs
